8d56d22b3f27e254ab2deec3ffc61a8d1c2b7f70
alivelamkon <alihaghani.abadani@gmail.com>
2026-07-28 18:55:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "✅ ربات کریپتو آلارم با موفقیت وصل شد!"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Message sent")
